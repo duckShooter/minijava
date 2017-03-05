1009f90f327a293fc88b761eea8497a45876c11e
Jo <a47_74_4774@aol.com>
2017-03-05 20:30:18 +0200

    a single step in a journey of a thousand miles

---
+++ src/compiler/analyzer/lex/LexicalEngine.java
@@ -0,0 +1,17 @@
+package compiler.analyzer.lex;
+
+//NOTE: this structure is eligible to change later
+
+/**
+ * <h3>LexicalEngine</h3>
+ * <p>An engine that processes and converts a stream of characters into lexical units using regex patterns</p>
+ */
+public class LexicalEngine {
+	
+	/**
+	 * Reads a line of input and scan it to extract lexemes
+	 */
+	public void scan() {
+		
+	}
+}
