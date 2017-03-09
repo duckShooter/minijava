62b3f1f1d4eb11c38f5870909d3ba7a6f03e4207
Andrew Albert <andrewa@silverkeytech.com>
2017-03-09 19:02:25 +0200

    added some regex

--- src/compiler/analyzer/lex/LexicalEngine.java
+++ src/compiler/analyzer/lex/LexicalEngine.java
@@ -1,17 +1,59 @@
 package compiler.analyzer.lex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 //NOTE: this structure is eligible to change later
 
-/**
- * <h3>LexicalEngine</h3>
- * <p>An engine that processes and converts a stream of characters into lexical units using regex patterns</p>
- */
 public class LexicalEngine {
-	
+	private static StringBuilder leximesStr;
+	private static ArrayList<Token> tokens;
+
 	/**
 	 * Reads a line of input and scan it to extract lexemes
 	 */
-	public void scan() {
+	public static void scan() {
+		tokens = Token.readFile("data.txt");
 		
+		//the string of all tokens
+		leximesStr = new StringBuilder();
+		
+		for(Token temp : tokens)
+			leximesStr.append(temp.regex + "|");
+		leximesStr.deleteCharAt(leximesStr.length() - 1);//removing the last '|'
+	    
+		Pattern pat = Pattern.compile(leximesStr.toString());
+		Matcher matcher = pat.matcher(readSyntax());
+		while(matcher.find()){
+			System.out.println(matcher.group());
+			//finish this part then
+		}
+	}
+	
+	public static String readSyntax(){
+		File file = new File("syntax.txt");
+		
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			byte[] data = new byte[(int) file.length()];
+			fis.read(data);
+			fis.close();
+			return new String(data, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		scan();
 	}
 }
---
+++ src/compiler/analyzer/lex/Token.java
@@ -0,0 +1,35 @@
+package compiler.analyzer.lex;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Token {
+	public String regex;
+	public String type;
+	public Token(String regex, String type){
+		this.regex = regex;
+		this.type = type;
+	}
+	
+	public static ArrayList<Token> readFile(String file){
+		try {
+			List<String> stringList = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
+			ArrayList<Token> tokens = new ArrayList<>();
+			String[]line;
+			for(String str : stringList){
+				line = str.split(" ");
+				tokens.add(new Token(line[1], line[0]));
+			//	System.out.println(line[0]);
+			}
+			return tokens;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
